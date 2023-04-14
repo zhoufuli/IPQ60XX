7f29728ddb6c0faeb2eec1e62461ac7596dca051
zhoufuli <62980080+zhoufuli@users.noreply.github.com>
2023-04-14 19:14:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/lu
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能使用中文）
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Lancet'' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Zhoufuli'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）；
 #sed -i "s/OpenWrt /迷宫旅人.ver $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
